8ed39d8b0ec0bd1189fd2e3d3918b7166782aebb
Mitali2700 <mitaligamer2002@gmail.com>
2023-01-23 23:38:30 +0530

    components and pages folder added with home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import './App.css';
+import Home from "./pages/Home"
 
 function App() {
   return (
+    // change App into app bcz of BAM naming convention
     <div className="app">
       <h1>Hey Manu! Let's build the search engine</h1>
+      
+      {/* Home */}
+      <Home />
+
+      {/* Search page */}
+
     </div>
+
+   
+   
+    
   );
 }
 
