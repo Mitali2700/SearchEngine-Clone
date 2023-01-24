e2bcbe376b48b18110dfd8720331846b9954bb3e
Mitali2700 <mitaligamer2002@gmail.com>
2023-01-24 12:58:12 +0530

    Materil ui dependencies installation done with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import './App.css';
 import Home from "./pages/Home"
+import Search from "./pages/Search"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     // change App into app bcz of BAM naming convention
-    <div className="app">
-      <h1>Hey Manu! Let's build the search engine</h1>
-      
-      {/* Home */}
-      <Home />
-
-      {/* Search page */}
-
-    </div>
+    // <div className="app">
+    // <h1>hello</h1>
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/search" element={<Search/>}/>
+      </Routes>
+    </BrowserRouter>
+     
+    // </div>
 
    
    
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Home.css';
+//import SearchIcon from '@mui/icons-material/Search';
 
 function Home() {
   return (
     <div className='home'>
         <h1>This is home page</h1>
+        <div className='home_header'>
+          <div className='home_headerLeft'>
+            {/* link */}
+            {/* link */}
+          </div>
+          <div className='home_headerRight'>
+            {/* link */}
+            {/* link */}
+            {/* Icon */}
+            {/* Avatar */}
+          </div>
+        </div>
+        <div className='home_body'>
+          
+        </div>
     </div>
   )
 }
