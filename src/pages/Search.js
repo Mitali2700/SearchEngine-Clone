de3803e63d33de3ee133f760bd4882d8cf5eba52
Mitali2700 <mitaligamer2002@gmail.com>
2023-02-04 21:09:14 +0530

    usestate hook added and return function updated

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import './Search.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
 import { Button } from '@mui/material';
 
 function Search() {
+  const [input, setInput] = useState('');
+  // State -> How we write variables in react
 
   const search = e =>{
     e.preventDefault(); //bxz of this function we don't need to refresh anything
+
+    console.log('You hit the search button');
+    // do something with input
   }
 
   return (
-    <div className='search'>
+    <form className='search'>
     {/* <h1>This is serch page</h1> */}
       <div className='search_input'>
           <SearchIcon className='search_inputIcon'/>
-          <input/>
+          <input value={input} onChange={e => setInput(e.target.value)}/>
           <MicIcon className='mic_Icon'/>         
       </div> 
 
       <div className='search_buttons'>
-            <Button onClick={search} variant="outlined">Torch Search</Button>
+            <Button type='submit' onClick={search} variant="outlined">Torch Search</Button>
             <Button variant="outlined">I'm feeling luckey</Button>
           </div> 
-    </div>
+    </form>
   )
 }
 
